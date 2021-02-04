52578d26190ee3a7ca14ea258b089b9828a8fcde
ArlletteFonseca <arllette.fonseca@gmail.com>
2021-02-04 04:45:42 +0000

    added routing

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import Home from './pages/home';
+import TeacherSearch from './pages/teacherSearch';
 
 export default class App extends React.Component {
   render() {
-    return <Home />;
+    return (
+      <div>
+        <div className="text-center">
+          <Link className="px-4" to="/">Home</Link>
+          <Link className="px-4" to="/teacherSearch">Teacher Search</Link>
+        </div>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/teacherSearch"><TeacherSearch/></Route>
+      </div>
+    );
   }
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -2,7 +2,37 @@ import React from 'react';
 
 export default function Home(props) {
   return (
-    <>
-    </>
+    <div className="container my-container d-flex flex-column justify-content-center align-items-center">
+      <div className="row my-row">
+        <div className="col my-column">
+          <h1 className="logo">CHAT BOX</h1>
+        </div>
+      </div>
+      <div className="row my-row">
+        <div className="col my-column">
+          <h4 className="heading">Teacher Login</h4>
+        </div>
+      </div>
+      <form className=" justify-content-center align-items-center w-70">
+        <div className="row  form-group">
+          <div className="col my-column">
+            <label className="label">Email address</label>
+            <input type="text" placeholder="Enter email" className="input"/>
+        </div>
+          </div>
+          <div className="row form-group">
+            <div className="col my-column">
+              <label className="label">Password</label>
+              <input type="text" placeholder="Enter password" className="input"/>
+        </div>
+            </div>
+            <div className="row form-group my-row">
+              <div className="col my-column ">
+                <button type="button" className="btn button">Teacher Login</button>
+              </div>
+            </div>
+    </form>
+
+        </div>
   );
 }
