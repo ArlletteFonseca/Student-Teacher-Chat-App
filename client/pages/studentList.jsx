e8d9c2a9b23c6895c47ca3f516a99f0418d08e3b
ArlletteFonseca <arllette.fonseca@gmail.com>
2021-02-10 05:48:01 +0000

    completed teacher can see info for students

--- client/pages/studentList.jsx
+++ client/pages/studentList.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 export default function studentList(props) {
   const listStudent = props.onChange.map(student =>
-    <ul className="list-group m-2" key={student.studentID}>
+
+    <ul className="list-group m-2 listWidth" key={student.studentID}>
       <li className="list-group-item d-flex justify-content-between align-items-center">
         <span className="badge bg-secondary badge-pill "><Link className=" studentLink" to={'/' + student.lastName }></Link>
         </span>
         <div className="d-flex justify-content-around align-items-center">
           <span className="name">{student.firstName}</span> <span className="name">{student.lastName}</span>
         </div>
-
       </li>
       <li className="list-group-item d-flex justify-content-end align-items-center ">
         {student.gradeLevel}
@@ -22,7 +22,8 @@ export default function studentList(props) {
   return (
     <div>
       <Link to='./teacherSearch'><i className="fas fa-chevron-left fa-2x back"></i></Link>
-      {listStudent}
+      <p className="m-2">Select Student</p>
+        {listStudent}
     </div>
   );
 }
--- client/pages/students/grant.jsx
+++ client/pages/students/grant.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function grant() {
+export default function brown() {
   return (
-    <div>
-      <p>hello, I am  grant</p>
+    <div className="container-fluid my-container d-flex flex-column justify-content-center align-items-center">
+      <div className="m-2 chatLink d-flex justify-content-start">
+        <Link to='./teacherSearch'><i className="fas fa-chevron-left fa-2x back"></i></Link>
+      </div>
+      <div className="card text-center ">
+        <div className="card-header">
+          <div className="card">
+            <img src='img/joshgrant2.jpeg' alt="Student Michael Brown" />
+          </div>
+        </div>
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">Josh Grant</h5>
+        <p className="card-text">Senior</p>
+        <a href="#" className="chatBtn btn-primary">Start Chat</a>
+      </div>
     </div>
   );
 }
