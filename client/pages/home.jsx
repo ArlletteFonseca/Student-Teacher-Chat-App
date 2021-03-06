e11bf3fd2d712e5fcbf81ede7ea7143e65eec0ec
ArlletteFonseca <arllette.fonseca@gmail.com>
2021-03-06 22:19:51 +0000

    fixed logo alignment

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export default function home(props) {
   return (
     <div className="container-fluid my-container d-flex flex-column justify-content-center align-items-center ">
-      <div className="row logo ">
+      <div className="row ">
         <div className="col ">
-           <img src='img/chatbox4.jpg' alt="chatbox logo" className="logo-img" />
+           <img src='img/chatbox4.jpg' alt="chatbox logo" className="logo" />
         </div>
       </div>
       <div className="row ">
