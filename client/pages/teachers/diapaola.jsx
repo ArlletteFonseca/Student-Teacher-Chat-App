ff6fa078d661ca871221b140dbe35d898d9b2845
ArlletteFonseca <arllette.fonseca@gmail.com>
2021-03-07 23:33:43 +0000

    arrow fixed due to spacing

--- client/pages/teachers/diapaola.jsx
+++ client/pages/teachers/diapaola.jsx
@@ -12,7 +12,7 @@ export default function brown(props) {
 
   return (
     <div className="container-fluid cardContainer d-flex flex-column justify-content-center">
-      <Link to='./studentSearch'><i className="fas fa-chevron-left fa-2x back back-arrow"></i></Link>
+      <Link to='./studentSearch' className="back"><i className="fas fa-chevron-left fa-2x back back-arrow"></i></Link>
       <div className="card cardContainer text-center d-flex flex-column justify-content-around align-items-center">
         <div className="card-header">
           <div className="card">
