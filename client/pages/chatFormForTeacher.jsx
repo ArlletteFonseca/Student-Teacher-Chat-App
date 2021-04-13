81749a1bb3e71dde9909f54e13420ce5d4d72945
ArlletteFonseca <arllette.fonseca@gmail.com>
2021-04-13 21:13:34 +0000

    working on keyboard and input for mobile

--- client/pages/chatFormForTeacher.jsx
+++ client/pages/chatFormForTeacher.jsx
@@ -131,10 +131,10 @@ export default class ChatForm extends React.Component {
 
         </div>
 
-     <div className="panel-footer fixed-input">
+     <div className="panel-footer ">
          <form onSubmit={this.handleSubmit}>
           <div className="input-group " >
-            <input id="btn-input" type="text" className="form-control input-sm" placeholder="Type your message here..." onChange={this.handleChange} />
+            <input id="btn-input" type="text" className="form-control input-sm fixed-input" placeholder="Type your message here..." onChange={this.handleChange} />
             <span className="input-group-btn">
               <button className="btn btn-warning" id="btn-chat" >Send</button>
             </span>
